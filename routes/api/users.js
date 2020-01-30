8af9b01dc3cd3eaf1f600fd1d8f5ba181ad2046c
henok <henokkahsay43@gmail.com>
2020-01-30 05:45:16 -0800

    second work

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,27 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-vlidator/check");
 
-router.get('/', (req, res) => res.send('user route'));
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    console.log(req.body);
 
+    if (!errors.isEmpty()) {
+      return res.status(400).jspn({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
-module.exports=router;
+module.exports = router;
