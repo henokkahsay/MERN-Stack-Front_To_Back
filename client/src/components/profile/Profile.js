92a2e91520470d8886c5d70e86891dc9dbfae198
henok <henokkahsay43@gmail.com>
2020-03-21 06:19:57 -0700

    ProfileTop & ProfileAbout Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileById, getCurrentProfile } from "../../actions/profile";
 import { Link } from "react-router-dom";
 
@@ -13,7 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getCurrentProfile]);
+  }, [getCurrentProfile, match.params.id]);
   return (
     <Fragment>
       {profile === null || loading ? (
@@ -30,6 +32,10 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
